aefb6dfc75f2208e197ba705dbb4393a819d742c
gnedivad <gnedivad@gmail.com>
2018-06-17 21:16:44 -0700

    [004] Updated some constants; added debugger

--- 004_gaussian_kernel.py
+++ 004_gaussian_kernel.py
@@ -9,7 +9,7 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string(
   "image_filename", "data/lenna.png", """Filename for image.""")
 tf.app.flags.DEFINE_boolean(
-  "show", False, """Whether to show or not.""")
+  "show", True, """Whether to show or not.""")
 
 ###############################################################################
 # Tensorflow implementation of scipy.ndimage.filters.gaussian_filter for      #
@@ -24,7 +24,7 @@ def expanded_gaussian_kernel(pixels, sigma):
     xx, yy = np.meshgrid(ax, ax)
     kernel = np.exp(-(xx**2 + yy**2) / (2. * sigma**2))
     return kernel / np.sum(kernel)
-  
+
   return np.expand_dims(
     np.expand_dims(gaussian_kernel(pixels, sigma), axis=2),
     axis=3
@@ -33,23 +33,26 @@ def expanded_gaussian_kernel(pixels, sigma):
 
 def main(argv=None):
   image = imread(FLAGS.image_filename, flatten=True)
-  blurred_image = gaussian_filter(image, sigma=2.0, order=0).astype(np.uint8)
-  
+  import pdb; pdb.set_trace()
+  blurred_image = gaussian_filter(
+    image, sigma=2.0, order=0
+  ).astype(np.int32)
+
   image_placeholder_op = tf.placeholder(
     tf.float32, shape=(512, 512), name="image")
   expanded_image_placeholder_op = tf.expand_dims(
     tf.expand_dims(image_placeholder_op, axis=0),
     axis=3
   )
-  # Use tf.nn.conv2d instead of tf.layers.conv2d since layers must be explicit
+  # Use tf.nn.conv2d instead of tf.layers.conv2d since filters must be explicit
   blurred_image_op = tf.cast(
     tf.nn.conv2d(
       expanded_image_placeholder_op,  # (1, 512, 512, 1)
-      expanded_gaussian_kernel(13, 2.0),  # (512, 512, 1, 1)
+      expanded_gaussian_kernel(5, 2.0),  # (512, 512, 1, 1)
       [1, 1, 1, 1],
       padding="SAME"
     ),  # (1, 512, 512, 1)
-    tf.uint8
+    tf.int32
   )
   squeezed_blurred_image_op = tf.squeeze(blurred_image_op)  # (512, 512)
   with tf.Session() as sess:
@@ -58,9 +61,10 @@ def main(argv=None):
       fetches={"blurred_image": squeezed_blurred_image_op},
       feed_dict={"image:0": image}
     )
-  
+
+  # print(np.sum(np.abs(blurred_image - result["blurred_image"])) / (512.**2))
   if FLAGS.show:
-    plt.imshow(blurred_image - result["blurred_image"], cmap="gray")
+    plt.imshow(np.abs(blurred_image - result["blurred_image"]), cmap="gray", vmin=0, vmax=255)
     plt.show()
 
 
